ddae2d03afa039a3e074166cc77fc14113bb5949
Alex2003763 <alex89646969@gmail.com>
2025-10-31 20:21:01 +0800

    Update PremiumBalanceCard.tsx

--- components/PremiumBalanceCard.tsx
+++ components/PremiumBalanceCard.tsx
@@ -165,19 +165,6 @@ const PremiumBalanceCard: React.FC<PremiumBalanceCardProps> = ({
         <div className="premium-card-back" aria-hidden={!isFlipped}>
           <div className="premium-card-back-header">
             <h3 className="back-title">Daily Summary</h3>
-            <button
-              onClick={(e) => {
-                e.stopPropagation();
-                handleFlip();
-              }}
-              className="back-close-button"
-              aria-label="Flip to front of card"
-              tabIndex={-1}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
-              </svg>
-            </button>
           </div>
 
           <div className="premium-card-summary">
@@ -206,5 +193,6 @@ const PremiumBalanceCard: React.FC<PremiumBalanceCardProps> = ({
     </div>
   );
 };
+
 
 export default PremiumBalanceCard;
