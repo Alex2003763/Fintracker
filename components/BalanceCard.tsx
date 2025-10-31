0e5b9ebe930dd1ec8902a34ac6d4273677139f98
Alex2003763 <alex89646969@gmail.com>
2025-10-31 20:08:43 +0800

    Refactor BalanceCard component structure

--- components/BalanceCard.tsx
+++ components/BalanceCard.tsx
@@ -102,11 +102,6 @@ const BalanceCard: React.FC<BalanceCardProps> = ({ transactions, onAddTransactio
         <div className="card-back bg-[rgb(var(--color-card-rgb))] text-[rgb(var(--color-text-rgb))] p-4 shadow-lg flex flex-col justify-between">
             <div className="flex justify-between items-start mb-3">
                 <h3 className="text-lg font-bold">Daily Summary</h3>
-                <button onClick={() => setIsFlipped(!isFlipped)} className="text-[rgb(var(--color-text-muted-rgb))] hover:text-[rgb(var(--color-text-rgb))] transition-colors" aria-label="Flip card">
-                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
-                     </svg>
-                </button>
             </div>
 
             <div className="flex-1 flex items-center justify-center">
@@ -139,5 +134,6 @@ const BalanceCard: React.FC<BalanceCardProps> = ({ transactions, onAddTransactio
     </div>
   );
 };
+
 
 export default BalanceCard;
