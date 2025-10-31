eee2207aa5f4b269af27d083ae462eceb8679479
Alex2003763 <alex89646969@gmail.com>
2025-10-31 20:25:20 +0800

    Update PremiumBalanceCard.tsx

--- components/PremiumBalanceCard.tsx
+++ components/PremiumBalanceCard.tsx
@@ -93,20 +93,7 @@ const PremiumBalanceCard: React.FC<PremiumBalanceCardProps> = ({
                   <span>{sign}{dailyChange.toFixed(1)}%</span>
                 </div>
               </div>
-              <button
-                className="premium-card-flip-button"
-                aria-label="Flip to back of card"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleFlip();
-                }}
-                tabIndex={-1}
-              >
-                <svg width="16" height="16" viewBox="0 0 20 20" fill="currentColor">
-                  <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                  <path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.012 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" />
-                </svg>
-              </button>
+          
             </div>
           </div>
           
@@ -165,6 +152,19 @@ const PremiumBalanceCard: React.FC<PremiumBalanceCardProps> = ({
         <div className="premium-card-back" aria-hidden={!isFlipped}>
           <div className="premium-card-back-header">
             <h3 className="back-title">Daily Summary</h3>
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+                handleFlip();
+              }}
+              className="back-close-button"
+              aria-label="Flip to front of card"
+              tabIndex={-1}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+              </svg>
+            </button>
           </div>
 
           <div className="premium-card-summary">
@@ -193,6 +193,5 @@ const PremiumBalanceCard: React.FC<PremiumBalanceCardProps> = ({
     </div>
   );
 };
-
 
 export default PremiumBalanceCard;
