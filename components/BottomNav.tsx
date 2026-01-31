555ac39f2b8284822aedf1ab7d08917b619a654b
Alex2003763 <alex89646969@gmail.com>
2026-01-31 14:16:52 +0800

    Update BottomNav.tsx

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -19,10 +19,6 @@ const BottomNav: React.FC<BottomNavProps> = ({ activeItem, setActiveItem }) => {
   return (
     <nav
       className="md:hidden fixed bottom-0 left-0 right-0 bg-[rgb(var(--color-card-rgb))] border-t border-[rgb(var(--color-border-rgb))] flex items-center justify-between z-10 transition-colors px-2"
-      style={{
-        paddingLeft: 'max(0.5rem, env(safe-area-inset-left))',
-        paddingRight: 'max(0.5rem, env(safe-area-inset-right))'
-      }}
     >
       <div className="flex flex-1 h-16 items-center">
         {navItems.map((item) => (
